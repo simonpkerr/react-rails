c7d781ae236e810956b0ba26ca257beb2857025b
Simon Kerr <simon.kerr@oegen.co.uk>
2016-09-16 09:08:52 +0100

    working simple example

---
+++ app/assets/frontend/greet.jsx
@@ -0,0 +1,5 @@
+export default class Greet extends React.Component {
+    render () {
+        return <h1>Hi react</h1>;
+    };
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './app/assets/frontend/main.jsx',
+  output: {
+    path: __dirname + '/app/assets/javascripts',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['','.js','.jsx']
+  },
+  module: {
+    loaders: [
+      { 
+        test: /\.jsx$/,
+        exclude: /node_modules/, 
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        } 
+      }
+    ]
+  },
+  // don't bundle react npm package, this is included in rails'
+  externals: {
+    'react': 'React'
+  }
+};
